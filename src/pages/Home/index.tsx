8bd1bc4ae30983d0d4b8d9cfb7e7a00bd376ced9
Alexander Lay-Calvert <alex.laycalvert@gmail.com>
2022-06-28 08:06:18 -0400

    first dev build of the website

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,48 @@
+import Typed from "react-typed";
+import "./index.scss";
+
+const Home: React.FC = () => {
+    return (
+        <div className="home">
+            <div className="home-title">
+                <div className="home-title-block">
+                    <div className="home-title-typed">
+                        <Typed
+                            strings={[
+                                "Developer",
+                                "Student",
+                                "U.S. Marine",
+                                "Alex Lay-Calvert",
+                            ]}
+                            typeSpeed={60}
+                            backSpeed={80}
+                            backDelay={1000}
+                        />
+                    </div>
+                    <div className="home-title-links">
+                        <a href="https://github.com/alex-laycalvert" target="#">
+                            <i className="fa-brands fa-github"></i>
+                        </a>
+                        <a
+                            href="https://www.linkedin.com/in/alexander-lay-calvert-2179501b4/"
+                            target="#"
+                        >
+                            <i className="fa-brands fa-linkedin"></i>
+                        </a>
+                        <a
+                            href="https://www.instagram.com/laycalva/"
+                            target="#"
+                        >
+                            <i className="fa-brands fa-instagram"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div className="home-content">
+                <h1>Coming soon to a web browser near you...</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
