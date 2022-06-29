1133c833ffbbbad06d4e36687523ef79aa84c3e7
Alexander Lay-Calvert <alex.laycalvert@gmail.com>
2022-06-29 18:47:49 -0400

    started working on graph

--- src/pages/Dev/Graph/index.tsx
+++ src/pages/Dev/Graph/index.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import GraphContainer from "../../../components/GraphContainer";
+import { GraphNode } from "../../../components/GraphContainer/Node";
+import "./index.scss";
 
 const Graph: React.FC = () => {
-    return <div>THIS IS A COOL GRAPH</div>;
+    const [graphNodeLayers, setGraphNodeLayers] = useState<GraphNode[][]>([]);
+
+    useEffect(() => {}, []);
+    return (
+        <div>
+            <h1>Graph</h1>
+            <p>
+                This is a proof of concept of graphing nodes onto a web app
+                screen
+            </p>
+            <div>
+                <GraphContainer nodeLayers={graphNodeLayers} />
+            </div>
+        </div>
+    );
 };
 
 export default Graph;
