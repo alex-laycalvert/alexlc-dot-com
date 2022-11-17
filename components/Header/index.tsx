01de287fe02ba397ab6bd98e3e94940bb87eab4e
Alexander Lay-Calvert <alex.laycalvert@gmail.com>
2022-11-17 18:56:54 -0500

    Nextjs implementation

---
+++ components/Header/index.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import Typewriter from "typewriter-effect";
+import Navbar from "../Navbar";
+
+import styles from "../../styles/Header.module.scss";
+
+export default function Header() {
+    const typedStrings = ["Developer", "U.S. Marine", "Linux Enthusiast", "Neovim User"];
+
+    return (
+        <header className={styles.ALHeader}>
+            <div className={styles.side}>
+                <div className={styles.imageWrapper}>
+                <Image
+                    src="/assets/images/logo.png"
+                    alt="AL Development Logo"
+                    width={75}
+                    height={75}
+                /></div>
+                <h1>Alex L-C</h1>
+                <div className={styles.dotSeparator}>.</div>
+                <div className={styles.typewriter}>
+                    <Typewriter
+                        options={{
+                            loop: true,
+                        }}
+                        onInit={(typewriter) => {
+                            typewriter.changeDelay(70);
+                            typewriter.changeDeleteSpeed(100);
+                            typedStrings.forEach((str) => {
+                                typewriter.typeString(str).pauseFor(900).deleteAll().pauseFor(250);
+                            });
+                            typewriter.start();
+                        }}
+                    />
+                </div>
+            </div>
+            <div className={`${styles.side} ${styles.right}`}>
+                <Navbar />
+            </div>
+        </header>
+    );
+}
---
+++ components/Navbar/index.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+
+import styles from "../../styles/Navbar.module.scss";
+
+type NavLink = {
+    name: string;
+    route: string;
+};
+
+export default function Navbar() {
+    const pages: NavLink[] = [
+        {
+            name: "Home",
+            route: "/",
+        },
+        {
+            name: "Projects",
+            route: "/projects",
+        },
+        {
+            name: "Clients",
+            route: "/clients",
+        },
+        {
+            name: "Resume",
+            route: "/resume",
+        },
+    ];
+
+    return (
+        <nav className={styles.ALNavbar}>
+            <ul className={styles.navLinks}>
+                {pages.map((page, i, arr) => {
+                    return (
+                        <>
+                            <li className={styles.navLink}>
+                                <Link href={page.route}>{page.name}</Link>
+                            </li>
+                            {i !== arr.length - 1 && <div className={styles.dotSeparator}>.</div>}
+                        </>
+                    );
+                })}
+            </ul>
+        </nav>
+    );
+}
