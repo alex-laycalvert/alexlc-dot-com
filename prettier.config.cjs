933cc82d43f61d42b9dacd3761e21030940b399e
Alex Lay-Calvert <alex.laycalvert@gmail.com>
2023-12-13 17:10:45 -0500

    working on it

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,14 @@
 /** @type {import("prettier").Config} */
 const config = {
-    plugins: [require.resolve("prettier-plugin-tailwindcss")],
+    plugins: [
+        require.resolve("prettier-plugin-astro"),
+        require.resolve("prettier-plugin-tailwindcss"),
+    ],
+    tabWidth: 4,
+    semi: true,
+    singleQuote: false,
+    arrowParens: "always",
+    trailingComma: "all",
 };
 
 module.exports = config;
